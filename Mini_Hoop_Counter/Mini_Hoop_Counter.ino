3a6304a8204930050ee3fc62b7d73fdb8e4887b2
advay <134825013+advay-c@users.noreply.github.com>
2023-07-05 20:37:08 -0400

    Add files via upload

---
+++ Mini_Hoop_Counter/Mini_Hoop_Counter.ino
@@ -0,0 +1,63 @@
+#include <LCD.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#define checkbit(x, y) ((x) & (y))
+#define bitn(p) (0x01 << (p))
+
+void trigger();
+
+int main(void)
+{
+  LCD lcd;
+  lcd.init();
+  int count;
+  double distance;
+  DDRB = 0x01;
+  int pinstate;
+  int objectCount = 0;
+
+  while (1)
+  {
+    trigger();
+
+    while (checkbit(PINB, bitn(1)))
+    {
+      TCCR1A = 0x00;
+      TCCR1B = 0x03;
+      pinstate = 1;
+    }
+
+    while (!checkbit(PINB, bitn(1)) && pinstate == 1)
+    {
+      TCCR1B = 0x00;
+      count = TCNT1;
+      distance = (count * 0.008575);
+
+      lcd.cmd(0x01);
+      lcd.cmd(0x0C);
+
+      if (distance < 4.0)
+      {
+        // Increment the object count
+        objectCount++;
+
+        // Display the object count on LCD
+        lcd.showvalue(objectCount);
+
+        _delay_ms(750);
+      }
+
+      pinstate = 0;
+      TCNT1 = 0;
+    }
+  }
+}
+
+void trigger()
+{
+  PORTB = 0x00;
+  _delay_us(10);
+  PORTB = 0x01;
+  _delay_us(10);
+  PORTB = 0x00;
+}
